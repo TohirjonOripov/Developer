1aa89072eb1a98bf7767a77bd7bd870ef381e5fc
TohirjonOripov <88825075+TohirjonOripov@users.noreply.github.com>
2021-08-12 14:09:11 +0500

    src

---
+++ data.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+        id:1,
+        title:"Frontent developer",
+        info:" html css Javascript React js Vue js",
+    },
+    {
+        id:2,
+        title:"Backend developer",
+        info:"Php yii2 laravel php frameworklari",
+    },
+    {
+        id:3,
+        title:"Mobil developer",
+        info:"React Native Python,Kotlin,Java",
+    }
+
+]
+export default questions;
