f17344ca0578e887d32218c6f906c3ab2b2b0371
TohirjonOripov <88825075+TohirjonOripov@users.noreply.github.com>
2021-09-16 16:07:02 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,81 +1,75 @@
-import React, { useState }  from 'react';
-import questions from './Components/data';
-import data from './Components/data';
-import SingleQuestion from './Components/Question';
-import './index.css';
-function App() {
-  const [question,setQuestion] = useState(data);
 
-  return(
-  <>
+import React from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+
+
+
+
+ function App() {
+
+
+  const [photo,setPhoto] = React.useState({});
+
+   let openPhoto = async function () {
+     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+     if (permissionResult.granted) {
+       let pickerResult = await ImagePicker.launchImageLibraryAsync();
+       if (pickerResult) {
+         setPhoto(pickerResult);
+       }
+     }
+     
+   }
+ async function sharePhoto(){
+         let isAvailable = await Sharing.isAvailableAsync()
+         if (isAvailable) {
+           console.log(photo.uri);
+           await Sharing.shareAsync(photo.uri);
+         }
+   }
+  return (
+    
+      <View style={styles.container}>
+      
+        <TouchableOpacity onPress={openPhoto} style={{
+          width:100,
+          height:50,
+          backgroundColor:'blue',
+          justifyContent: 'center',
+          alignItems:'center',
+          borderRadius:10
+          
+        }}>
+        <Text style={{
+          color:"white"
+        }}>File Yuklash</Text>
+        </TouchableOpacity>
+        
+    
+        <View>
+       { photo && (
+      <TouchableOpacity onPress={e => sharePhoto()}>
+           <Image source={{ uri: photo.uri }} style={{width:359, height:440}}/>
+      </TouchableOpacity>
+      )}
+    </View>    
   
-  <div className="container">
-    <h2>Question and Answers</h2>
+      </View>
+    
 
-    <section className="info">
-      {questions.map((question) => {
-        return <SingleQuestion key={question.id} {...question}></SingleQuestion>;
-      })}
-
-    </section>
-
-  </div>
-  
-  </>
 
 
   );
-
-
-
 }
-  // const[count, setCount] = useState(0);
-  // useEffect(()=>{
-  //   document.title = `siz ${count} marta bosdingiz`;
 
-  // })
-
-  //     return(
-  //    <div>
-  //       <h1 align="center">
-  //         {count}
-  //       </h1>
-
-  //       <button onClick={() => setCount(count + 1)}>Plus</button>
-
-  //    </div>
-      
-  //   );
-  // }
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 export default App;
-
-// import React, { Component } from 'react';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 0
-//     };
-//   }
-//   componentDidMount() {
-//     document.title = `You clicked ${this.state.count} times`;
-//   }
-//   componentDidUpdate() {
-//     document.title = `You clicked ${this.state.count} times`;
-//   }
-//   render() {
-//     return (
-//       <div>
-//           <p>You clicked {this.state.count} times</p>
-//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-//           Click me
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
